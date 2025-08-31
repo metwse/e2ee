3c6deb9a9bd0a9ddad34e459b109cddfe63762b0
Metehan Selvi <me@metehanselvi.com>
2025-08-31 18:52:14 +0300

    reafactor provider interface, tests: hkdf

--- e2ee/src/error.rs
+++ e2ee/src/error.rs
@@ -3,4 +3,5 @@
 /// This type used if the spesific reasons for a failure are obvious or are not
 /// useful to know, or providing more details about the error is dangereous due
 /// to side channel attacks.
+#[derive(Debug)]
 pub struct Unspecified;
